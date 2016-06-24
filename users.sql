85c01be9285466dd0eb6e279404fc8b5f3e0c3e2
Mahedi <mahedi0244@gmail.com>
2016-06-24 11:58:56 -0400

    Logout option is added

--- users.sql
+++ users.sql
@@ -23,13 +23,30 @@ INSERT INTO users (username, password) VALUES ('Mahedi', '1234'),
 ('Redwon', '1234'),
 ('Jack', '1234');
 
-INSERT INTO toDoList (user_id, taskname, description) VALUES ('1', 'Homework', 'Due on Monday'),
-('1', 'Bank Payment','Due on Tuesday'),
-('2', 'Homework', 'Due on Tuesday'),
-('3', 'Doctors appoinment', 'Due on Monday'),
-('3', 'Homework', 'Due on Tuesday'),
-('3', 'Car Payment', 'Due on Monday'),
-('4', 'Loan Payment', 'Due on Friday');
+INSERT INTO toDoList (user_id, taskname, description) VALUES ('1', 'Homework', 'Due on 07/26/2016'),
+('1', 'Bank Payment','Due on 07/28/2016'),
+('1', 'Mortgage Payment','Due on 07/29/2016'),
+('1', 'CS 316 Project','Due on 08/01/2016'),
+('1', 'Car Payment','Due on 08/05/2016'),
+('1', 'Doctor appoinment','Due on 08/10/2016'),
+('2', 'Homework', 'Due on 07/26/2016'),
+('2', 'Bank Payment','07/28/2016'),
+('2', 'Mortgage Payment','Due on 07/29/2016'),
+('2', 'CS 316 Project','Due on 08/01/2016'),
+('2', 'Car Payment','Due on 08/05/2016'),
+('2', 'Doctor appoinment','Due on 08/10/2016'),
+('3', 'Homework', 'Due on 07/26/2016'),
+('3', 'Bank Payment','07/28/2016'),
+('3', 'Mortgage Payment','Due on 07/29/2016'),
+('3', 'CS 316 Project','Due on 08/01/2016'),
+('3', 'Car Payment','Due on 08/05/2016'),
+('3', 'Doctor appoinment','Due on 08/10/2016'),
+('4', 'Homework', 'Due on 07/26/2016'),
+('4', 'Bank Payment','07/28/2016'),
+('4', 'Mortgage Payment','Due on 07/29/2016'),
+('4', 'CS 316 Project','Due on 08/01/2016'),
+('4', 'Car Payment','Due on 08/05/2016'),
+('4', 'Doctor appoinment','Due on 08/10/2016')
 
 
 
